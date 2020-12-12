1f459158d8c5dbf48614d47d883d409fb42f384c
Antoine Rieuneau <ar3119@ic.ac.uk>
2020-12-12 23:00:42 +0400

    Updated RK4 for 4 simultanious equations + created output file and added equations. Still not working (illegal operation produces ind restults after first iteration)

--- Coursework1/main.cpp
+++ Coursework1/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 using namespace std;
+
+
+/* https://www.youtube.com/watch?v=JECaqGiKUXg
+https://math.stackexchange.com/questions/1594617/difficulty-using-excel-to-solve-the-double-pendulum-problem-using-rk4-to-solve-f */
 
 
 int main() {
 
-    const float g = 9.81;
+    const double g = 9.81;
 
 
 
@@ -43,28 +48,72 @@ int main() {
         cout << "Unable to open file";
     }
 
+    ofstream resultsFile("output.txt"); //write results in overwritten output.txt file
+    resultsFile << "t\t\tx\t\ty\n";
+    resultsFile << "---------------------------------\n";
+
+
 
     //Runge Kutta 4 Method
+    // x = Theta1, u= Theta2, y= dTheta1/dt v=dTheta2/dt
+    double ma1 = stof(parameters[0]);
+    double ma2 = stof(parameters[1]);
+    double l1 = stof(parameters[2]);
+    double l2 = stof(parameters[3]);
+    double x = stof(parameters[4]); //x0
+    double u = stof(parameters[5]); //u0
+    
+    double T = 0;
+    double v = 0;
+    double y = 0;
 
-#define f1(t,theta1) 0
-#define f2(t, theta2) 0
+#define f1(T,x,u,y,v) y
+#define f2(T,x,u,y,v) v
+#define f3(T,x,u,y,v) (-g*(2*ma1+ma2)*sin(x)-ma2*g*sin(x-2u)-2*sin(x-u)*ma2*(pow(v,2)*l2+pow(y,2)*l1*cos(x-u)))/(l1*(2*ma1+ma2-ma2*cos(2*x-2*u)))
+#define f4(T,x,u,y,v) (2*sin(x-u)*(pow(y,2)*l1*(ma1+ma2)+g*(ma1+ma2)*cos(x)+pow(v,2)*l2*ma2*cos(x-u))) / (l2*(2*ma1+ma2-ma2*cos(2*x-2*u)))
 
-    float t0, t10, y0;
-    float h = stoi(parameters[6]);
-    float n = stoi(parameters[7]) / h;
+    double h = stof(parameters[6]);
+    double n = 0.02 / h;//stof(parameters[7]) / h;
+
+
+    resultsFile << T << "\t" << x << "\t" << u << endl;
+
 
     for (int i = 1; i < n; i++) {
-        float k1 = h * (f1(t0, t10));
-        float k2 = h * (f1(t0+0.5*k1, t10+h/2));
-        float k3 = h * (f1(t0 + 0.5 * k2, t10 + h / 2));
-        float k4 = h * (f1(t0+k3,t10 +h));
-        float y = t0 + (k1 + k2) / 6 + (k2 + k3) / 3 + 0;
-        t0 = t0 + h;
-        y0 = y;
+
+        double k1 = h * (f1(T,x,u,y,v));
+        double l1 = h * (f2(T, x, u, y, v));
+        double m1 = h * (f3(T, x, u, y, v));
+        double o1 = h * (f4(T, x, u, y, v));
+
+        double k2 = h * (f1(T + 0.5 * h, x + 0.5 * h * k1, u + 0.5 * h * l1, y + 0.5 * h * m1, v + 0.5 * h * o1));
+        double l2 = h * (f2(T + 0.5 * h, x + 0.5 * h * k1, u + 0.5 * h * l1, y + 0.5 * h * m1, v + 0.5 * h * o1));
+        double m2 = h * (f3(T + 0.5 * h, x + 0.5 * h * k1, u + 0.5 * h * l1, y + 0.5 * h * m1, v + 0.5 * h * o1));
+        double o2 = h * (f4(T + 0.5 * h, x + 0.5 * h * k1, u + 0.5 * h * l1, y + 0.5 * h * m1, v + 0.5 * h * o1));
+
+        double k3 = h * (f1(T + 0.5 * h, x + 0.5 * h * k2, u + 0.5 * h * l2, y + 0.5 * h * m2, v + 0.5 * h * o2));
+        double l3 = h * (f2(T + 0.5 * h, x + 0.5 * h * k2, u + 0.5 * h * l2, y + 0.5 * h * m2, v + 0.5 * h * o2));
+        double m3 = h * (f3(T + 0.5 * h, x + 0.5 * h * k2, u + 0.5 * h * l2, y + 0.5 * h * m2, v + 0.5 * h * o2));
+        double o3 = h * (f4(T + 0.5 * h, x + 0.5 * h * k2, u + 0.5 * h * l2, y + 0.5 * h * m2, v + 0.5 * h * o2));
+
+        double k4 = h * (f1(T + h, x + h * k3, u + h * l3, y + h * m3, v + h * o3));
+        double l4 = h * (f2(T + h, x + h * k3, u + h * l3, y + h * m3, v + h * o3));
+        double m4 = h * (f3(T + h, x + h * k3, u + h * l3, y + h * m3, v + h * o3));
+        double o4 = h * (f4(T + h, x + h * k3, u + h * l3, y + h * m3, v + h * o3));
+        
+        u = u + (k1 + k4) / 6 + (k2 + k3) / 3;
+        x = x + (l1 + l4) / 6 + (l2 + l3) / 3;
+        y = y + (m1 + m4) / 6 + (m2 + m3) / 3;
+        v = v + (o1 + o4) / 6 + (o2 + o3) / 3;
+
+        cout << l1 << " " << l2 << " " << l3 << " " << l4 << endl; //returns ind after l2 - probably illegal opperation on a float
+        resultsFile << T << "\t" << x << "\t" << u << endl;
+
+        T = T + h;
     }
 
 
-    ofstream resultsFile("output.txt"); //write results in overwritten output.txt file 
+    
     resultsFile.close();
 
     return 0;
