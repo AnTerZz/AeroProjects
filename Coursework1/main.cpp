6b9fbf6066ace5b666d2218f4fb6d3d086ceda88
Antoine Rieuneau <ar3119@ic.ac.uk>
2021-01-21 00:43:27 +0000

    Working code - needs optimisation but all functionnality is there

--- Coursework1/main.cpp
+++ Coursework1/main.cpp
@@ -7,30 +7,30 @@ using namespace std;
 
 /* https://www.youtube.com/watch?v=JECaqGiKUXg
 https://math.stackexchange.com/questions/1594617/difficulty-using-excel-to-solve-the-double-pendulum-problem-using-rk4-to-solve-f 
-https://www.math24.net/double-pendulum/#:~:text=Substituting%20this%20in%20the%20original,l2%CE%B122.&text=ddt%E2%88%82L%E2%88%82%CB%99%CE%B11%E2%88%92%E2%88%82,L%E2%88%82%CE%B12%3D0.*/
+https://www.math24.net/double-pendulum/#:~:text=Substituting%20this%20in%20the%20original,l2%CE%B122.&text=dh%E2%88%82L%E2%88%82%CB%99%CE%B11%E2%88%92%E2%88%82,L%E2%88%82%CE%B12%3D0. */
 
-double f1(double T, double x, double u, double y, double v);
-double f2(double T, double x, double u, double y, double v);
-double f3(double T, double x, double u, double y, double v, string parameters[]);
-double f4(double T, double x, double u, double y, double v, string parameters[]);
-double* fun(double T, double x, double u, double y, double v, string parameters[]);
+const int g_g = 9.81;
+
+double f1(double t, double theta1, double theta2, double theta11, double theta22, double params[]);
+double f2(double t, double theta1, double theta2, double theta11, double theta22, double params[]);
+double f3(double t, double theta1, double theta2, double theta11, double theta22, double params[]);
+double f4(double t, double theta1, double theta2, double theta11, double theta22, double params[]);
+
+
 
 int main() {
 
-    const double g = 9.81;
 
-
-
-    string parameters[8]; //consider using a struct for code readability
-    ifstream cParams("parameters.txt"); 
+    double parameters[8]; //consider using a struct for code readability
+    ifstream cParams("parameters.txt");
 
     if (!cParams) { //check if the parameters file exists
 
         cout << "The parameters file was not found in the directory and has been created with default values" << endl;
         ofstream wParams;
-        wParams.open("parameters.txt"); //creates a bland parameters file and writes default values to is as follows
+        wParams.open("parameters.txt"); //creates a blank parameters file and writes default values to is as follows
         wParams << "1.5 "; //m1
-        wParams << "2.0 "; //m2
+        wParams << "2 "; //m2
         wParams << "1.0 "; //l1
         wParams << "1.0 "; //l2
         wParams << "1.571 "; //theta10
@@ -45,7 +45,7 @@ int main() {
 
         for (int i = 0; i < 8; i++) {
             fParams >> parameters[i]; //store values from the file into an array of 8 strings
-          cout << "Read parameter " << i << " : " << parameters[i] << endl; 
+            cout << "Read parameter " << i << " : " << parameters[i] << endl;
         }
         fParams.close();
     }
@@ -61,82 +61,61 @@ int main() {
 
 
     //Runge Kutta 4 Method
-    // x = Theta1, u= Theta2, y= dTheta1/dt v=dTheta2/dt
-    double ma1 = stof(parameters[0]);
-    double ma2 = stof(parameters[1]);
-    double l1 = stof(parameters[2]);
-    double l2 = stof(parameters[3]);
-    double x = stof(parameters[4]);  //x0
-    double u = stof(parameters[5]);  //u0
-    
-    double T = 0;
-    double v = 0;
-    double y = 0;
+    // x = Theta1 x1, u= Theta2 x2, y= dTheta1/h y1, v=dTheta2/h y2
+    double m1 = parameters[0];
+    double m2 = parameters[1];
+    double l1 = parameters[2];
+    double l2 = parameters[3];
+    double theta1 = parameters[4];  //x0
+    double theta2 = parameters[5];  //u0
+    double dTheta1 = 0;
+    double dTheta2 = 0;
+    double h = parameters[6];
+    double n = parameters[7] / h;
 
-/*#define f1(T,x,u,y,v) y
-#define f2(T,x,u,y,v) v
-#define f3(T,x,u,y,v) (-g*(2*ma1+ma2)*sin(x)-ma2*g*sin(x-2u)-2*sin(x-u)*ma2*(pow(v,2)*l2+pow(y,2)*l1*cos(x-u)))/(l1*(2*ma1+ma2-ma2*cos(2*x-2*u)))
-#define f4(T,x,u,y,v) (2*sin(x-u)*(pow(y,2)*l1*(ma1+ma2)+g*(ma1+ma2)*cos(x)+pow(v,2)*l2*ma2*cos(x-u))) / (l2*(2*ma1+ma2-ma2*cos(2*x-2*u)))*/
+    double k11, k12, k13, k14, k21, k22, k23, k24, k31, k32, k33, k34, k41, k42, k43, k44, x2, y2;
 
-    double h = stof(parameters[6]);
-    double n = stof(parameters[7]) / h;
+    double t = 0;
 
 
-    resultsFile << T << "\t" << x << "\t" << u << endl;
+    while (t <= 10) {
+        k11 = f2(t, theta1, theta2, dTheta1, dTheta2, parameters);
+        k12 = f1(t, theta1, theta2, dTheta1, dTheta2, parameters);
+        k13 = f4(t, theta1, theta2, dTheta1, dTheta2, parameters);
+        k14 = f3(t, theta1, theta2, dTheta1, dTheta2, parameters);
 
 
-    for (int i = 1; i < n; i++) {
-
-        //cout << f1(T, x, u, y, v) << " " << f2(T, x, u, y, v) << " " << f3(T, x, u, y, v,parameters) << " " << f4(T, x, u, y, v,parameters) << endl;
-
-
-        /*double k1 = h * (f1(T,x,u,y,v));
-        double l1 = h * (f2(T, x, u, y, v));
-        double m1 = h * (f3(T, x, u, y, v,parameters));
-        double o1 = h * (f4(T, x, u, y, v,parameters));*/
+        k21 = f2((t + h / 2), (theta1 + k12 * h / 2), (theta2 + k14 * h / 2), (dTheta1 + k11 * h / 2), (dTheta2 + k13 * h / 2), parameters);
+        k22 = f1((t + h / 2), (theta1 + k12 * h / 2), (theta2 + k14 * h / 2), (dTheta1 + k11 * h / 2), (dTheta2 + k13 * h / 2), parameters);
+        k23 = f4((t + h / 2), (theta1 + k12 * h / 2), (theta2 + k14 * h / 2), (dTheta1 + k11 * h / 2), (dTheta2 + k13 * h / 2), parameters);
+        k24 = f3((t + h / 2), (theta1 + k12 * h / 2), (theta2 + k14 * h / 2), (dTheta1 + k11 * h / 2), (dTheta2 + k13 * h / 2), parameters);
 
 
-        double* res1 = fun(T,x,u,y,v,parameters);
+        k31 = f2((t + h / 2), (theta1 + k22 * h / 2), (theta2 + k24 * h / 2), (dTheta1 + k21 * h / 2), (dTheta2 + k23 * h / 2), parameters);
+        k32 = f1((t + h / 2), (theta1 + k22 * h / 2), (theta2 + k24 * h / 2), (dTheta1 + k21 * h / 2), (dTheta2 + k23 * h / 2), parameters);
+        k33 = f4((t + h / 2), (theta1 + k22 * h / 2), (theta2 + k24 * h / 2), (dTheta1 + k21 * h / 2), (dTheta2 + k23 * h / 2), parameters);
+        k34 = f3((t + h / 2), (theta1 + k22 * h / 2), (theta2 + k24 * h / 2), (dTheta1 + k21 * h / 2), (dTheta2 + k23 * h / 2), parameters);
 
 
-
-        /*double k2 = h * (f1(T + 0.5 * h, x + 0.5 * h * k1, u + 0.5 * h * l1, y + 0.5 * h * m1, v + 0.5 * h * o1));
-        double l2 = h * (f2(T + 0.5 * h, x + 0.5 * h * k1, u + 0.5 * h * l1, y + 0.5 * h * m1, v + 0.5 * h * o1));
-        double m2 = h * (f3(T + 0.5 * h, x + 0.5 * h * k1, u + 0.5 * h * l1, y + 0.5 * h * m1, v + 0.5 * h * o1));
-        double o2 = h * (f4(T + 0.5 * h, x + 0.5 * h * k1, u + 0.5 * h * l1, y + 0.5 * h * m1, v + 0.5 * h * o1,parameters));*/
-
-        double* res2 = fun(T + 0.5 * h, x + 0.5 * h * res1[0], u + 0.5 * h * res1[1], y + 0.5 * h * res1[2], v + 0.5 * h * res1[3], parameters);
-
-        /*double k3 = h * (f1(T + 0.5 * h, x + 0.5 * h * k2, u + 0.5 * h * l2, y + 0.5 * h * m2, v + 0.5 * h * o2));
-        double l3 = h * (f2(T + 0.5 * h, x + 0.5 * h * k2, u + 0.5 * h * l2, y + 0.5 * h * m2, v + 0.5 * h * o2));
-        double m3 = h * (f3(T + 0.5 * h, x + 0.5 * h * k2, u + 0.5 * h * l2, y + 0.5 * h * m2, v + 0.5 * h * o2,parameters));
-        double o3 = h * (f4(T + 0.5 * h, x + 0.5 * h * k2, u + 0.5 * h * l2, y + 0.5 * h * m2, v + 0.5 * h * o2,parameters));*/
-
-        double* res3 = fun(T + 0.5 * h, x + 0.5 * h * res2[0], u + 0.5 * h * res2[1], y + 0.5 * h * res2[2], v + 0.5 * h * res2[3], parameters);
+        k41 = f2((t + h), (theta1 + k32 * h), (theta2 + k34 * h), (dTheta1 + k31 * h), (dTheta2 + k33 * h), parameters);
+        k42 = f1((t + h), (theta1 + k32 * h), (theta2 + k34 * h), (dTheta1 + k31 * h), (dTheta2 + k33 * h), parameters);
+        k43 = f4((t + h), (theta1 + k32 * h), (theta2 + k34 * h), (dTheta1 + k31 * h), (dTheta2 + k33 * h), parameters);
+        k44 = f3((t + h), (theta1 + k32 * h), (theta2 + k34 * h), (dTheta1 + k31 * h), (dTheta2 + k33 * h), parameters);
 
 
-        /*double k4 = h * (f1(T + h, x + h * k3, u + h * l3, y + h * m3, v + h * o3));
-        double l4 = h * (f2(T + h, x + h * k3, u + h * l3, y + h * m3, v + h * o3));
-        double m4 = h * (f3(T + h, x + h * k3, u + h * l3, y + h * m3, v + h * o3,parameters));
-        double o4 = h * (f4(T + h, x + h * k3, u + h * l3, y + h * m3, v + h * o3,parameters));*/
+        dTheta1 = dTheta1 + (k11 + k21 * 2 + k31 * 2 + k41) / 6.0 * h;
+        theta1 = theta1 + (k12 + k22 * 2 + k32 * 2 + k42) / 6.0 * h;
+        dTheta2 = dTheta2 + (k13 + k23 * 2 + k33 * 2 + k43) / 6.0 * h;
+        theta2 = theta2 + (k14 + k24 * 2 + k34 * 2 + k44) / 6.0 * h;
 
-        double* res4 = fun(T + 0.5 * h, x + 0.5 * h * res3[0], u + 0.5 * h * res3[1], y + 0.5 * h * res3[2], v + 0.5 * h * res3[3], parameters);
 
-        
-        /*u = u + (k1 + k4) / 6 + (k2 + k3) / 3;
-        x = x + (l1 + l4) / 6 + (l2 + l3) / 3;
-        y = y + (m1 + m4) / 6 + (m2 + m3) / 3;
-        v = v + (o1 + o4) / 6 + (o2 + o3) / 3;*/
+        x2 = l1*sin(theta1) + l2 * sin(theta2);
+        y2 = -l1*cos(theta1) - l2 * cos(theta2);
 
-        u = u + (res1[0] + res4[0]) / 6 + (res2[0] + res3[0]) / 3;
-        x = x + (res1[1] + res4[1]) / 6 + (res2[1] + res3[1]) / 3;
-        y = y + (res1[2] + res4[2]) / 6 + (res2[2] + res3[2]) / 3;
-        v = v + (res1[3] + res4[3]) / 6 + (res2[3] + res3[3]) / 3;
 
-        //cout << k1 << " " << k2 << " " << k3 << " " << k4 << endl; //returns ind after l2 - probably illegal opperation on a float
-        resultsFile << T << "\t" << x << "\t" << u << endl;
-
-        T = T + h;
+        t += h;
+        cout << t << " " << x2 << " " << y2 << endl;
+        resultsFile << t << "\t" << x2 << "\t" << y2 << endl;
     }
 
 
@@ -145,47 +124,22 @@ int main() {
     return 0;
 }
 
- double f1(double T,double x,double u,double y,double v) {
-    double res = y;
-    return res;
+
+double f1(double t, double theta1, double theta2, double theta11, double theta22,double params[]) {
+    return theta11;
 }
 
- double f2(double T, double x, double u, double y, double v) {
-     double res = v;
-     return res;
- }
 
- double f3(double T, double x, double u, double y, double v, string parameters[]) {
-     const double g = 9.81;
-     double ma1 = stof(parameters[0]);
-     double ma2 = stof(parameters[1]);
-     double l1 = stof(parameters[2]);
-     double l2 = stof(parameters[3]);
-     double res = (-g * (2 * ma1 + ma2) * sin(x) - ma2 * g * sin(x - 2u) - 2 * sin(x - u) * ma2 * (pow(v, 2) * l2 + pow(y, 2) * l1 * cos(x - u))) / (l1 * (2 * ma1 + ma2 - ma2 * cos(2 * x - 2 * u)));
-     return res;
- }
+double f2(double t, double theta1, double theta2, double theta11, double theta22, double params[]) {
+    return ((-g_g * (2.0 * params[0] + params[1]) * sin(theta1) - params[1] * g_g * sin(theta1 - 2.0 * theta2) - 2.0 * sin(theta1 - theta2) * params[1] * (pow(theta22, 2) * params[3] + pow(theta11, 2) * params[2] * cos(theta1 - theta2))) / (params[2] * (2.0 * params[0] + params[1] - params[1] * cos(2.0 * theta1 - 2.0 * theta2))));
+}
 
- double f4(double T, double x, double u, double y, double v, string parameters[]) {
-     const double g = 9.81;
-     double ma1 = stof(parameters[0]);
-     double ma2 = stof(parameters[1]);
-     double l1 = stof(parameters[2]);
-     double l2 = stof(parameters[3]);
-     double res = (2 * sin(x - u) * (pow(y, 2) * l1 * (ma1 + ma2) + g * (ma1 + ma2) * cos(x) + pow(v, 2) * l2 * ma2 * cos(x - u))) / (l2 * (2 * ma1 + ma2 - ma2 * cos(2 * x - 2 * u)));
-     return res;
- }
 
- double* fun(double T, double x, double u, double y, double v,string parameters[]) {
-     const double g = 9.81;
-     double ma1 = stof(parameters[0]);
-     double ma2 = stof(parameters[1]);
-     double l1 = stof(parameters[2]);
-     double l2 = stof(parameters[3]);
-     double h = stof(parameters[6]);
-     static double res[4];
-     res[0] =h * y; //k
-     res[1] =h* v; //l
-     res[2] =h* (-g * (2 * ma1 + ma2) * sin(x) - ma2 * g * sin(x - 2u) - 2 * sin(x - u) * ma2 * (pow(v, 2) * l2 + pow(y, 2) * l1 * cos(x - u))) / (l1 * (2 * ma1 + ma2 - ma2 * cos(2 * x - 2 * u)));
-     res[3] =h* (2 * sin(x - u) * (pow(y, 2) * l1 * (ma1 + ma2) + g * (ma1 + ma2) * cos(x) + pow(v, 2) * l2 * ma2 * cos(x - u))) / (l2 * (2 * ma1 + ma2 - ma2 * cos(2 * x - 2 * u)));
-     return res;
- }
+double f3(double t, double theta1, double theta2, double theta11, double theta22, double params[]) {
+    return theta22;
+}
+
+
+double f4(double t, double theta1, double theta2, double theta11, double theta22, double params[]) {
+    return((2.0 * sin(theta1 - theta2) * (pow(theta11, 2) * params[2] * (params[0] + params[1]) + g_g * (params[0] + params[1]) * cos(theta1) + pow(theta22, 2) * params[3] * params[1] * cos(theta1 - theta2))) / (params[3] * (2.0 * params[0] + params[1] - params[1] * cos(2.0 * theta1 - 2.0 * theta2))));
+}
